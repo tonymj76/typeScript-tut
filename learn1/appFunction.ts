e5a72b8f9fa632db40d90a4b35c53564fb3ecf9e
tonymj76 <demeyortj@gmail.com>
2020-12-10 15:04:15 +0100

    typescript functions

---
+++ learn1/appFunction.ts
@@ -0,0 +1,23 @@
+function add(n1: number, n2: number) {
+  return n1 + n2
+}
+
+//this function return void because it don't return any value
+function printResult(num: number): void{
+  console.log('Result: ' + num);
+}
+
+printResult(add(3,5))
+
+//function types are types that decribes a function
+let combineValus: (a: number, b: number) => number;
+
+combineValus = add;
+// combineValus = printResult; errored
+
+
+function addAndHandle(n1:number, n2: number, cb: (num: number) => void) {
+  cb(n1+n2);
+};
+
+addAndHandle(10, 20, (result) =>{console.log(result)})
