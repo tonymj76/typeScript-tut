e93d3a1e91d32b3b6947a72cca2f1fcb29956e1f
tonymj76 <demeyortj@gmail.com>
2020-12-14 11:24:05 +0100

    interface

--- learn1/app.ts
+++ learn1/app.ts
@@ -1,28 +1,17 @@
-// unknow type
+//Interfacings
 
-let userInput: unknown;
-let userName : string;
-
-userInput = 5;
-userInput = 'max';
-if (typeof userInput === 'string') {
-  userName = userInput;
+// it describe the structure of an object will look like
+// interface can't have a default value
+interface Person {
+  name: string;
+  age: number;
+  greet(phrase: string): void;
 }
-
-//Never type
-
-function generateError(message: string, code: number): never {
-  throw {message, code};
-  // note throw always end our script remedy is to use try and catch
-  // while(true) {} --> never return a value
-}
-
-// generateError("An error occured!", 500)
-
-const btn = document.querySelector("button")!;
-
-function clickHandler(message: string){
-  console.log("hello " + message)
-}
-
-btn.addEventListener('click', clickHandler.bind(null, "John"));
+// interface person as a type
+let user1: Person = {
+  name: 'Max',
+  age: 33,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name)
+  }
+};
