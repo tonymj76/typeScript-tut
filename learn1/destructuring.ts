014d21d439b99325bfb74dd0c69ec7069b5dce95
tonymj76 <demeyortj@gmail.com>
2020-12-14 11:23:54 +0100

    how to destructor

---
+++ learn1/destructuring.ts
@@ -0,0 +1,18 @@
+const addz = (a: number, b: number) => a+b;
+
+//Declaration of function
+const printableNumber: (a: number | string) => void = v => console.log(v)
+
+// Execution of the function
+printableNumber(addz(3,4))
+
+const b = document.querySelector('button')!;
+b.addEventListener("click", event => console.log(event))
+
+let composite: (...agrs: number[]) => number;
+const many = (...args: number[]) => args.reduce((c, p) => c+p)
+const many2: (...args: number[]) => number = (...d) => d.reduce((c, p)=> c+p)
+
+composite = many2
+
+printableNumber(composite(3,4,3,2,5))
