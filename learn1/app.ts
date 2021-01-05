e0380cf18dee91c9086e3b155c79ee383d1cf447
tonymj76 <demeyortj@gmail.com>
2021-01-05 11:00:43 +0100

    starting app

--- learn1/app.ts
+++ learn1/app.ts
@@ -1,16 +1,1 @@
-/*
-  Generic in TypeScript
-  generic is a type which is connected to another type which is flexible on how that type is computed
-*/
-
-const names: Array<string | number> = [];
-
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("this is done!")
-  }, 2000);
-})
-
-promise.then( data => {
-  data.split(" ")
-})
+console.log("Learning TypeScript");
