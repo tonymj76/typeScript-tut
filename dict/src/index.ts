d32b54655549604b1896c3895c19658b588192b0
tonymj76 <demeyortj@gmail.com>
2021-03-02 12:48:17 +0100

    map dict implementation

---
+++ dict/src/index.ts
@@ -0,0 +1,21 @@
+export type Dict<T> = {
+  [k: string]: T | undefined
+};
+
+// Array.prototype.map, but for Dict
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn:(arg:T, idx:number)=> S): Dict<S> {
+    const out: Dict<S> = {}
+    Object.keys(dict).forEach((dkey, idx) => {
+      const thisItem = dict[dkey]
+      if (typeof thisItem !== 'undefined'){
+        out[dkey] = fn(thisItem, idx)
+      }
+    })
+    return out
+  }
+mapDict({a: "a", b: "b"}, (str) => [str]);
+mapDict({a: "a", b: "b"}, (str) => ({val: str}))
+// Array.prototype.reduce, but for Dict
+export function reduceDict() {}
